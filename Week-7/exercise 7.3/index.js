61aa5a1f16a0b68f3913df4a4d8de4cb218053ed
joeshajan <joeshajan1551@gmail.com>
2022-12-24 16:52:09 +0530

    sort array of  0's 1's and 2's

---
+++ Week-7/exercise 7.3/index.js
@@ -0,0 +1,30 @@
+var sort = function (nums) {
+  let low = 0;
+  let mid = 0;
+  let high = nums.length - 1;
+
+  while (mid <= high) {
+    // if mid = 0 swap mid to low
+    if (nums[mid] === 0) {
+      let temp = nums[low];
+      nums[low] = nums[mid];
+      nums[mid] = temp;
+      low++;
+      mid++;
+    } else if (nums[mid] === 1) {
+      // if mid = 1 increment mid
+      mid++;
+    } else {
+      // if mid = 2 swap with high
+      let temp = nums[high];
+      nums[high] = nums[mid];
+      nums[mid] = temp;
+      high--;
+    }
+  }
+
+  return nums;
+};
+
+console.log(sort([0, 2, 1, 2, 0])); // [ 0, 0, 1, 2, 2 ]
+console.log(sort([0, 1, 0])); // [ 0, 0, 1 ]
