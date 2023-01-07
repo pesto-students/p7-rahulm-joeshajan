1f8af369b73d40baa0581adb9be48d8a096e4cd5
joeshajan <joeshajan1551@gmail.com>
2023-01-07 12:10:41 +0530

    3 sum optimized

--- Week-7/exercise 7.6/index.js
+++ Week-7/exercise 7.6/index.js
@@ -1,20 +1,16 @@
 function find3Numbers(arr, target) {
-  let obj = {};
   let diff = 1000000;
   let closest = 0;
 
   for (let i = 0; i < arr.length - 2; i++) {
     for (let j = i + 1; j < arr.length - 1; j++) {
-      obj[`${arr[i]}, ${arr[j]}, ${arr[j + 1]}`] = arr[i] + arr[j] + arr[j + 1];
+      let value = arr[i] + arr[j] + arr[j + 1];
+      if (diff > Math.abs(value - target)) {
+        diff = Math.abs(value - target);
+        closest = value;
+      }
     }
   }
-
-  Object.values(obj).forEach((value) => {
-    if (diff > Math.abs(value - target)) {
-      diff = Math.abs(value - target);
-      closest = value;
-    }
-  });
   return closest;
 }
 
